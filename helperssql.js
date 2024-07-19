bdd693ceb853869e9b100d7665024ed23b483c87
cloudedvr <148506764+cloudedvr@users.noreply.github.com>
2024-07-19 09:53:30 -0700

    Add files via upload

---
+++ helperssql.js
@@ -0,0 +1,15 @@
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new Error("No data");
+
+  const cols = keys.map((colName, idx) =>
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
